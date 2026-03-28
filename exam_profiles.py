33fca52a681b566dbea17ac22e32645d56cd5b21
JotaJ1991 <163599426+JotaJ1991@users.noreply.github.com>
2026-03-28 15:30:28 -0500

    Update exam_profiles.py

--- exam_profiles.py
+++ exam_profiles.py
@@ -110,7 +110,7 @@ SIPAGRE_1S = {
     ],
 
     'answers_top_f':    0.176,
-    'answers_bottom_f': 0.975,
+    'answers_bottom_f': 0.976,
     'bubble_radius':     9,
     'fill_threshold':   0.06,
     'min_contrast':     0.05,
