2859d9f7d48fd2c1d2d840319b1beb3a849426cf
JotaJ1991 <163599426+JotaJ1991@users.noreply.github.com>
2026-03-28 15:25:42 -0500

    Update exam_profiles.py

--- exam_profiles.py
+++ exam_profiles.py
@@ -109,8 +109,8 @@ SIPAGRE_1S = {
          'timing_fx': 0.746},
     ],
 
-    'answers_top_f':    0.177,
-    'answers_bottom_f': 0.97,
+    'answers_top_f':    0.176,
+    'answers_bottom_f': 0.975,
     'bubble_radius':     9,
     'fill_threshold':   0.06,
     'min_contrast':     0.05,
